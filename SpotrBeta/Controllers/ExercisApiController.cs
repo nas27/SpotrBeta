2c4e2deb4f9c16dc827268ff29c51c825581a983
Chris Di Conza <christopher.diconza@sheridanc.on.ca>
2016-11-08 12:00:29 -0500

    Configured our MVC project to have Web API functionality. Added 3 ApiControllers. I have successfully used GET from postman using all 3.
    This webapi change will be needed to communicate with the other database

---
+++ SpotrBeta/Controllers/ExercisApiController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SpotrBeta.Models;
+
+namespace SpotrBeta.Controllers
+{
+    public class ExercisApiController : ApiController
+    {
+        private SpotrContext db = new SpotrContext();
+
+        // GET: api/ExercisApi
+        public IQueryable<Exercis> GetExercises()
+        {
+            return db.Exercises;
+        }
+
+        // GET: api/ExercisApi/5
+        [ResponseType(typeof(Exercis))]
+        public IHttpActionResult GetExercis(int id)
+        {
+            Exercis exercis = db.Exercises.Find(id);
+            if (exercis == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(exercis);
+        }
+
+        // PUT: api/ExercisApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutExercis(int id, Exercis exercis)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != exercis.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(exercis).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExercisExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ExercisApi
+        [ResponseType(typeof(Exercis))]
+        public IHttpActionResult PostExercis(Exercis exercis)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Exercises.Add(exercis);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = exercis.Id }, exercis);
+        }
+
+        // DELETE: api/ExercisApi/5
+        [ResponseType(typeof(Exercis))]
+        public IHttpActionResult DeleteExercis(int id)
+        {
+            Exercis exercis = db.Exercises.Find(id);
+            if (exercis == null)
+            {
+                return NotFound();
+            }
+
+            db.Exercises.Remove(exercis);
+            db.SaveChanges();
+
+            return Ok(exercis);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ExercisExists(int id)
+        {
+            return db.Exercises.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ SpotrBeta/Controllers/WorkoutsApiController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SpotrBeta.Models;
+
+namespace SpotrBeta.Controllers
+{
+    public class WorkoutsApiController : ApiController
+    {
+        private SpotrContext db = new SpotrContext();
+
+        // GET: api/WorkoutsApi
+        public IQueryable<Workout> GetWorkouts()
+        {
+            return db.Workouts;
+        }
+
+        // GET: api/WorkoutsApi/5
+        [ResponseType(typeof(Workout))]
+        public IHttpActionResult GetWorkout(int id)
+        {
+            Workout workout = db.Workouts.Find(id);
+            if (workout == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(workout);
+        }
+
+        // PUT: api/WorkoutsApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutWorkout(int id, Workout workout)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != workout.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(workout).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WorkoutExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/WorkoutsApi
+        [ResponseType(typeof(Workout))]
+        public IHttpActionResult PostWorkout(Workout workout)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Workouts.Add(workout);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = workout.Id }, workout);
+        }
+
+        // DELETE: api/WorkoutsApi/5
+        [ResponseType(typeof(Workout))]
+        public IHttpActionResult DeleteWorkout(int id)
+        {
+            Workout workout = db.Workouts.Find(id);
+            if (workout == null)
+            {
+                return NotFound();
+            }
+
+            db.Workouts.Remove(workout);
+            db.SaveChanges();
+
+            return Ok(workout);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool WorkoutExists(int id)
+        {
+            return db.Workouts.Count(e => e.Id == id) > 0;
+        }
+    }
+}
